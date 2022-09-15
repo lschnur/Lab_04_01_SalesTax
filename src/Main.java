f7d20002e31a0390b4cbae1f2aacda7cabb28e85
schnurluke <schnurle@mail.uc.edu>
2022-09-15 16:33:48 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+  int price =2500;
+  double tax= price * 0.05;
+  double totalP= price + tax;
+        System.out.println("The cost of your tax is "+ tax + " The cost of your total price is "+ totalP);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
